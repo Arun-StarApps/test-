63f9e6051f639d09ea9c5a7585fda71a14df1311
Arun_kumar_47000 <arun@starapps.studio>
2023-07-26 12:04:17 +0530

    added check for mirror server

--- lib/log_generator/railtie.rb
+++ lib/log_generator/railtie.rb
@@ -3,7 +3,7 @@
 module LogGenerator
     class Railtie < Rails::Railtie
         config.after_initialize do
-            if defined?(Rails::Server) && Rails.const_defined?('Server')
+            if defined?(Rails::Server) && Rails.const_defined?('Server') && ENV["IS_MIRROR"].present?
               # Code to run your rake task here
               puts "Invoking command========="
               Rake::Task['clockwork:send_log'].invoke
