e19d44483e598a90afd416cbc5918805bfacb524
Arun-StarApps <136087280+Arun-StarApps@users.noreply.github.com>
2023-07-26 12:06:59 +0530

    removed print statements

--- lib/log_generator/railtie.rb
+++ lib/log_generator/railtie.rb
@@ -5,10 +5,9 @@ module LogGenerator
         config.after_initialize do
             if defined?(Rails::Server) && Rails.const_defined?('Server') && ENV["IS_MIRROR"].present?
               # Code to run your rake task here
-              puts "Invoking command========="
               Rake::Task['clockwork:send_log'].invoke
             end
           end
     end
   end
-  
+  .
