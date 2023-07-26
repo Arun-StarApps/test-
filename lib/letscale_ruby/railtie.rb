90aaf2f9d806ebb4385e2eed30527c12563e8f10
Arun_kumar_47000 <arun@starapps.studio>
2023-07-26 17:34:24 +0530

    initial commit

--- lib/letscale_ruby/railtie.rb
+++ lib/letscale_ruby/railtie.rb
@@ -1,12 +1,12 @@
 
 
-module LetscaleRuby
-    class Railtie < Rails::Railtie
-        config.after_assets_precompile do
-            if ENV["IS_MIRROR"].present? && ENV["LOG_TOKEN"].present?
-              # Code to run your rake task here
-              Rake::Task['clockwork:send_log'].invoke 
-            end
-          end
-    end
-  end
+# module LetscaleRuby
+#     class Railtie < Rails::Railtie
+#         config.after_assets_precompile do
+#             if ENV["IS_MIRROR"].present? && ENV["LOG_TOKEN"].present?
+#               # Code to run your rake task here
+#               Rake::Task['clockwork:send_log'].invoke 
+#             end
+#           end
+#     end
+#   end
